f3f3a125fadf47c45427f8d787a30ad2e545ece3
Cinthia M. Orona <devrider2021@gmail.com>
2024-09-23 07:08:31 -0300

    Ejercicio terminado

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # Express_Practica
 En este repositorio iremos subiendo la práctica de JavaScript y Express Js con Node que iremos realizando en las clases
+
+-----
+#### Ejericio 01 - Repositorio Github
+1) Clonarse el repo de la clase 06, donde hicimos las prácticas de rutas.
+(https://github.com/GZifts11/Express_Practica/tree/Clase_06_Express_Routing) 
+Recordá que cuando clonas el repo, el head está en la rama master.
+2) Moverte a la rama Clase_”06_Express_Routing”
+3) Generarse un Repositorio público su cuenta de GitHub que se llame
+Practica_Express_Rutas.
+4) Generar una rama develop en el repositorio.
+5) En la rama develop subir los cambios que descargaste del repo de la clase 06.
+6) Generar una nueva rama desde develop donde subirás tus cambios de esta 
+ejercitación.
+
+#### Ejericio 02 - Express - Rutas
+1) Generar un endpoint, con método GET, con la ruta /api/lenguajes/backend/ 
+Al invocar esta ruta del servidor, me debe traer todos los lenguajes de backend.
+2) Generar un endpoint, con método GET, con la ruta /api/lenguajes/backend/
+Que reciba por URL param, el parámetro "lenguaje".
+Al invocar esta ruta del servidor, me debe traer los lenguajes de backend que coincidan el 
+valor buscado.
+3) Generar un endpoint, con método GET, con la ruta /api/lenguajes/backend/
+Que reciba por URL param, el parámetro "turno".
+Al invocar esta ruta del servidor, me debe traer los lenguajes de backend que coincidan 
+con el turno buscado.
+4) Generar un endpoint, con método GET, con la ruta /api/lenguajes/frontend/
+Que reciba por URL param, el parámetro "turno".
+Al invocar esta ruta del servidor, me debe traer los lenguajes de frontend que coincidan 
+con el turno buscado.
+5) Generar un endpoint, con método GET, con la ruta /api/lenguajes/backend/
+Que reciba por URL param, el parámetro "cantidadAlumnos".
+Al invocar esta ruta del servidor, me debe traer los lenguajes de backend tengan la misma 
+cantidad de alumnos, o más, que la que se pasó por parámetro.
+6) Generar un endpoint, con método GET, con la ruta /api/lenguajes/frontend/
+Que reciba por URL param, el parámetro "cantidadAlumnos".
+Al invocar esta ruta del servidor, me debe traer los lenguajes de frontend tengan la misma 
+cantidad de alumnos, o más, que la que se pasó por parámetro.
+7) Generar un endpoint, con método GET, con la ruta /api/lenguajes/
+Que reciba por URL param, el parámetro "cantidadAlumnos".
+Al invocar esta ruta del servidor, me debe traer TODOS los lenguajes que tengan la misma 
+cantidad de alumnos, o más, que la que se pasó por parámetro.
--- server.js
+++ server.js
@@ -35,6 +35,11 @@ app.get('/api/lenguajes/frontend', (req, res) => {
     res.send(JSON.stringify(infoLenguajes.frontend));
 })
 
+app.get('/api/lenguajes/backend', (req, res) => {
+    res.send(JSON.stringify(infoLenguajes.backend));
+})
+
+
 app.get('/api/lenguajes/frontend/:lenguaje', (req, res) => {
     const lenguaje = req.params.lenguaje.toLocaleLowerCase();
     console.log(` el lenguaje que recibe por parametro es: ${lenguaje}`)
@@ -43,13 +48,106 @@ app.get('/api/lenguajes/frontend/:lenguaje', (req, res) => {
         lenguajes => lenguajes.nombre.toLocaleLowerCase() === lenguaje
     )
 
-    if(filtrado.length === 0){
+    if (filtrado.length === 0) {
         return res.status(404).send(`No se encontró el curso con lenguaje: ${lenguaje}`)
     }
 
     res.status(200).send(filtrado);
-
 })
+
+app.get('/api/lenguajes/backend/:lenguaje', (req, res) => {
+    const lenguaje = req.params.lenguaje.toLocaleLowerCase();
+    const filtrado = infoLenguajes.backend.filter(nombre => nombre.nombre.toLocaleLowerCase() === lenguaje);
+
+    if (filtrado.length === 0) {
+        return res.status(404).send(`No se encontró el curso con lenguaje: ${lenguaje}`)
+    }
+    res.status(200).send(filtrado);
+})
+
+
+app.get('/api/lenguajes/frontend/turno/:turno', (req, res) => {
+    const turnoParam = req.params.turno.toLocaleLowerCase();
+    const filtrado = infoLenguajes.frontend.filter(lenguaje => {
+        return lenguaje.turno && lenguaje.turno.toLocaleLowerCase() === turnoParam;
+    });
+
+    // Si no se encuentra ningún lenguaje, enviar un mensaje de error
+    if (filtrado.length === 0) {
+        return res.status(404).send({ mensaje: 'No se encontraron lenguajes de frontend para el turno especificado' });
+    }
+
+    res.send(filtrado);
+})
+
+
+app.get('/api/lenguajes/backend/turno/:turno', (req, res) => {
+    const turnoParam = req.params.turno.toLocaleLowerCase();
+    const filtrado = infoLenguajes.backend.filter(lenguaje => {
+        return lenguaje.turno && lenguaje.turno.toLocaleLowerCase() === turnoParam;
+    });
+
+    // Si no se encuentra ningún lenguaje, enviar un mensaje de error
+    if (filtrado.length === 0) {
+        return res.status(404).send({ mensaje: 'No se encontraron lenguajes de backend para el turno especificado' });
+    }
+
+    res.send(filtrado);
+})
+
+app.get('/api/lenguajes/frontend/alumnos/:cantidadAlumnos', (req,res) => {
+    let cantidadParam = parseInt(req.params.cantidadAlumnos, 10);
+    const filtrado = infoLenguajes.frontend.filter(valor => valor.cantidadAlumnos >= cantidadParam)
+
+    if(isNaN(cantidadParam)){
+        return res.status(400).send(`El valor ingresado no es un numero`)
+    }
+
+    if(filtrado.length === 0){
+        return res.status(404).send(`No se encontraron lenguajes de frontend con ${cantidadParam} alumnos`);
+    }
+
+    res.status(200).send(filtrado);    
+})
+
+app.get('/api/lenguajes/backend/alumnos/:cantidadAlumnos', (req, res) => {
+    let cantidadParam = parseInt(req.params.cantidadAlumnos, 10);
+    const filtrado = infoLenguajes.backend.filter(valor => valor.cantidadAlumnos >= cantidadParam)
+
+    if (isNaN(cantidadParam)) {
+        return res.status(400).send(`El valor ingresado no es un numero`)
+    }
+
+    if (filtrado.length === 0) {
+        return res.status(404).send(`No se encontraron lenguajes de backend con ${cantidadParam} alumnos`);
+    }
+
+    res.status(200).send(filtrado);
+})
+
+app.get('/api/lenguajes/alumnos/:cantidadAlumnos', (req, res) => {
+    let cantidadParam = parseInt(req.params.cantidadAlumnos, 10);
+    const filtradoBack = infoLenguajes.backend.filter(valor => valor.cantidadAlumnos >= cantidadParam);
+    const filtradoFront = infoLenguajes.frontend.filter(valor => valor.cantidadAlumnos >= cantidadParam);
+
+    const joinLenguajes = filtradoBack.concat(filtradoFront);
+
+    if (isNaN(cantidadParam)) {
+        return res.status(400).send(`El valor ingresado no es un numero`)
+    }
+
+    if (joinLenguajes.length === 0) {
+        return res.status(404).send(`No se encontraron lenguajes con ${cantidadParam} alumnos`);
+    }
+
+    res.status(200).send(joinLenguajes);
+})
+
+
+
+
+
+
 
 
 app.listen(PORT, HOSTNAME, () => {
